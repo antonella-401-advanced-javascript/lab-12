2bb74fd87be9326e4bd33a5165f4a04bbbbac1dd
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-10-09 17:16:29 -0700

    second test passes

--- __tests__/read-file.test.js
+++ __tests__/read-file.test.js
@@ -1,10 +1,20 @@
-const { readFile } = require('../read-file');
+const { readFile, writeFile } = require('../read-file');
 
 describe('fs testing', () => {
   it('reads a file', () => {
     return readFile('./test-text.txt')
       .then(body => {
         expect(body).toEqual('only for testing purposes');
+      });
+  });
+
+  it('writes a new file', () => {
+    return writeFile('./write-test.txt', 'I hope this thing works')
+      .then(() => {
+        return readFile('./write-test.txt');
+      })
+      .then(body => {
+        expect(body).toEqual('I hope this thing works');
       });
   });
 });
